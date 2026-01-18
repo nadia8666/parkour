b2f9b15b2269b608d59de97a461905dca0c315ca
nadia8666 <115858781+nadia8666@users.noreply.github.com>
2026-01-18 14:57:07 -0800

    add debug mode switch

--- Assets/Code/Server/ENV.ts
+++ Assets/Code/Server/ENV.ts
@@ -1,6 +1,12 @@
+import { Airship } from "@Easy/Core/Shared/Airship";
+import { Binding } from "@Easy/Core/Shared/Input/Binding";
+
 const ENV: { Runtime: "DEV" | "PROD"; DebugDrawing: boolean } = {
 	Runtime: "DEV",
 	DebugDrawing: false,
 };
+
+Airship.Input.CreateAction("Debug", Binding.Key(Key.P));
+Airship.Input.OnDown("Debug").Connect(() => (ENV.DebugDrawing = !ENV.DebugDrawing));
 
 export default ENV;
