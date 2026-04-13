8405b84e360f5c542e59b3ed0cba411666e7ce16
nadia <115858781+nadia8666@users.noreply.github.com>
2026-04-13 15:58:00 -0700

    update model building

--- Assets/Code/Shared/Utility/ModelBuilder.ts
+++ Assets/Code/Shared/Utility/ModelBuilder.ts
@@ -10,10 +10,10 @@ export namespace ModelBuilder {
 		VoxelBlock,
 		VoxelPrefab,
 	}
+
 	export function BuildItemModel(Item: AnyItem) {
 		let ItemMesh: Mesh | undefined;
 
-		// TODO: reimplement
 		switch (Item.Type) {
 			case ItemTypes.Gear: {
 				ItemMesh = Asset.LoadAsset(`Assets/Resources/Models/Item/None.asset`);
@@ -29,14 +29,54 @@ export namespace ModelBuilder {
 				const Definition = Core().World.GetDefinitionFromBlock(Item.BlockID);
 				switch (Definition.ModelType) {
 					case BlockModel.Box: {
-						//const Mesh = MeshProcessor.ProduceSingleBlock(Item.BlockID, Core().World.World, 0, 1);
-						return $tuple(ModelBuilderType.VoxelBlock, undefined as unknown as GameObject, undefined, undefined);
+						const Object = GameObject.Create("VoxelBlock");
+						const Renderer = Object.AddComponent<MeshRenderer>();
+						const Filter = Object.AddComponent<MeshFilter>();
+						const TargetMesh = Filter.mesh;
+						TargetMesh.subMeshCount = 6;
+
+						const verts: Vector3[] = [];
+						const uvs: Vector2[] = [];
+
+						const addFace = (p0: Vector3, p1: Vector3, p2: Vector3, p3: Vector3) => {
+							verts.push(p0, p1, p2, p3);
+							uvs.push(new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1), new Vector2(1, 0));
+						};
+
+						addFace(new Vector3(-0.5, 0.5, -0.5), new Vector3(-0.5, 0.5, 0.5), new Vector3(0.5, 0.5, 0.5), new Vector3(0.5, 0.5, -0.5));
+						addFace(new Vector3(-0.5, -0.5, 0.5), new Vector3(-0.5, -0.5, -0.5), new Vector3(0.5, -0.5, -0.5), new Vector3(0.5, -0.5, 0.5));
+						addFace(new Vector3(0.5, -0.5, 0.5), new Vector3(0.5, 0.5, 0.5), new Vector3(-0.5, 0.5, 0.5), new Vector3(-0.5, -0.5, 0.5));
+						addFace(new Vector3(0.5, -0.5, 0.5), new Vector3(0.5, 0.5, 0.5), new Vector3(0.5, 0.5, -0.5), new Vector3(0.5, -0.5, -0.5));
+						addFace(new Vector3(-0.5, -0.5, -0.5), new Vector3(-0.5, 0.5, -0.5), new Vector3(0.5, 0.5, -0.5), new Vector3(0.5, -0.5, -0.5));
+						addFace(new Vector3(-0.5, -0.5, -0.5), new Vector3(-0.5, 0.5, -0.5), new Vector3(-0.5, 0.5, 0.5), new Vector3(-0.5, -0.5, 0.5));
+
+						TargetMesh.SetVertices(verts);
+						TargetMesh.SetUVs(0, uvs);
+
+						for (let i = 0; i < 6; i++) {
+							const base = i * 4;
+							TargetMesh.SetTriangles([base, base + 1, base + 2, base, base + 2, base + 3], i);
+						}
+
+						TargetMesh.RecalculateNormals();
+
+						Renderer.materials = [
+							Definition.GetTextureFor(Vector3.up),
+							Definition.GetTextureFor(Vector3.down),
+							Definition.GetTextureFor(Vector3.forward),
+							Definition.GetTextureFor(Vector3.right),
+							Definition.GetTextureFor(Vector3.back),
+							Definition.GetTextureFor(Vector3.left),
+						];
+
+						return $tuple(ModelBuilderType.VoxelBlock, Object, Filter, Renderer);
 					}
-					case BlockModel.Prefab: {
-						//const Model = Instantiate(Definition.prefab);
-						return $tuple(ModelBuilderType.VoxelPrefab, undefined as unknown as GameObject, undefined, undefined);
-					}
-					case BlockModel.None:
+
+					case BlockModel.Prefab:
+						return $tuple(ModelBuilderType.VoxelPrefab, Instantiate(Definition.Prefab), undefined, undefined);
+
+					default:
+						break;
 				}
 			}
 		}
